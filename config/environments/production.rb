7c42505c9831964e7da3f0db41a06e31450c82e6
rachaellynn <rachael@wordaroundtown.com>
2016-03-16 10:39:59 -0400

    tried again on assets/serving static files

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -22,7 +22,8 @@ Rails.application.configure do
 
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
-  config.serve_static_files = ENV['RAILS_SERVE_STATIC_FILES'].present?
+  # config.serve_static_files = ENV['RAILS_SERVE_STATIC_FILES'].present? # removing this for heroku deployment
+  config.serve_static_files = true
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
@@ -36,7 +37,7 @@ Rails.application.configure do
   config.assets.digest = true
 
   #added in because assets not coming through in Heroku deployment
-  config.serve_static_assets = true
+
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
