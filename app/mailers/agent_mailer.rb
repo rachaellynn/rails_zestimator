64ebcef912f99b86ae5b1d09191de997e723d03c
rachaellynn <rachael@wordaroundtown.com>
2016-03-31 11:55:02 -0400

    updated agent mailer to alter subject line and send to both agents and info

--- app/mailers/agent_mailer.rb
+++ app/mailers/agent_mailer.rb
@@ -21,6 +21,7 @@ class AgentMailer < ApplicationMailer
     #select agent to email to
     @@agent_id ||= 1 #set initial agent id
     agent_hash = { 1 => 'rachael@eplacehomes.com', 2 => 'rachaellburger@gmail.com'}
+    #agent_hash = { 1 => 'herb@eplacehomes.com', 2 => 'jasmin.lucci@eplacehomes.com', 3 => 'jennifer.keenan@eplacehomes.com', 4 => 'john.turner@eplacehomes.com', 5 => 'michael.pfister@eplacehomes.com', 6 => 'ryan.pietras@eplacehomes.com', 7 => 'victor.divine@eplacehomes.com'}
     @assigned_agent = agent_hash[@@agent_id]
     @@agent_id < 2? @@agent_id += 1: @@agent_id = 1
     
@@ -33,8 +34,11 @@ class AgentMailer < ApplicationMailer
     @zestimate.update_attributes(assigned_agent: @assigned_agent)
     #email_with_name = %("#{@user.name}" <#{@user.email}>)
     #mail(to: email_with_name, subject: 'Welcome to My Awesome Site')
-  	mail(to: @assigned_agent, subject: 'Home Valuation Lead')
-    mail(to: 'rachaellburger@gmail.com',subject: 'HVL for ' + @assigned_agent) #sends a copy of the email to Denise
+    (@market_report == 1 || @agent_contact == 1)? @subject = 'URGENT Home Valuation Lead': @subject = 'Home Valuation Lead'
+  	#mail(to: @assigned_agent, subject: @subject)
+    mail(to: "#{@assigned_agent}, rachaellburger+test@gmail.com", subject: @subject)
+    #mail(to: 'rachaellburger@gmail.com',subject: 'HVL for ' + @assigned_agent) #sends a copy of the email to Denise
+    ## use info there
   end
 
 end
